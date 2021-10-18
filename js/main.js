d75883eca9b55ece65b0f6a8273b1ed16da1160b
Klim_somov <klim_somov@mail.ru>
2021-10-18 19:45:31 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+$('.photo__slider').slick({
+    
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/Arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/Arrow-right.svg" alt=""></button>'
+});
+});
